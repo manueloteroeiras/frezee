f6679995a6787d6b91a84f45aaae24782c0472f3
Manuel <manuel.otero@line64.com>
2015-02-27 13:32:20 -0300

    neutro

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/jaeger.Html5Video/www/Html5Video.js",
+        "id": "jaeger.Html5Video.Html5Video",
+        "clobbers": [
+            "plugins.html5Video"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.device": "0.2.4",
+    "jaeger.Html5Video": "1.0.0"
+}
+// BOTTOM OF METADATA
+});
